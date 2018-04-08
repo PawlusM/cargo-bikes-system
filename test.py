33b2aba0a5930adc8879cf9be0d1d3514467d3e1
Vitalii Naumov <nau@debian>
2018-04-08 19:15:55 +0200

    experiment run

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ sender_code = size**2
 s_weight = stochastic.Stochastic(law=1, location=0.03, scale=0.005)
 s_dist = stochastic.Stochastic(law=0, location=0.05, scale=0.15)
 
+f = open("tw_diag.txt")
 total_TW = []
 for _ in range(100):
     n = net.Net()
@@ -19,7 +20,10 @@ for _ in range(100):
             clients.append(nd)
     n.gen_requests(sender=n.get_node(sender_code), nodes=clients, prob=0.5, s_weight=s_weight)
     rts = n.clarke_wright(sender_code=sender_code, requests=n.demand, capacity=0.15)
-    total_TW.append(sum([rt.transport_work for rt in rts]))
+    tw = sum([rt.transport_work for rt in rts])
+    total_TW.append(tw)
+    f.write(str(tw) + "\n")
 
+f.close()
 plt.hist(total_TW)
 plt.show()
